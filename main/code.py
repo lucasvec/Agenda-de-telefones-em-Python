3805c6700c20e7baf019e63eba8a90fd75d8b4fc
Lucas Vechiez Oliveira <83770905+lucasvec@users.noreply.github.com>
2022-10-30 12:57:51 -0300

    update/modificação para a postagem.

--- main/code.py
+++ main/code.py
@@ -1,12 +1,21 @@
+import time
+
 def mostrarMenu():
     global escolha_menu
     print('-- MENU -- ')
+    print('0 - Mostrar agenda.')
     print('1 - Adicionar contato.')
-    print('2 - Editar número de um contato existente.')
-    print('3 - Excluir número de contato existente.')
-    print('4 - Excluir contato.')
-    print('5 - Mostrar agenda.')
-    escolha_menu = int(input('R: '))
+    print('2 - Adicionar número em contato existente.')
+
+    while True:
+        escolha_menu = int(input('R: '))
+        if escolha_menu >= 0 or escolha_menu <= 2:
+            print('\n')
+            break
+        else:
+            print('Erro! Escolha as opções válidas.')
+            continue
+
 
 def pegarInformacoes():
     global quantidade_telefone
@@ -14,7 +23,7 @@ def pegarInformacoes():
     global numeros
     numeros = []
     dados = {}
-    dados['nome'] = str(input('Nome: '))
+    dados['nome'] = str(input('Nome: ')).title()
     quantidade_telefone = int(input('Quantos números você irá adicionar: '))
 
 def incluirNovoTel(dados, numeros, quantidade_telefone, agenda):
@@ -25,25 +34,37 @@ def incluirNovoTel(dados, numeros, quantidade_telefone, agenda):
     agenda.append(dados)
 
 def incluirTel(dados, numeros):
-    nome_existente = str(input('Nome: '))
+    nome_existente = str(input('Nome: ')).title()
     for i in agenda:
         for k, v in i.items():
             if nome_existente == v:
-                print('O nome tem, vamos adicionar um novo número.')
+                print('Vamos adicionar um novo número.')
                 novo_numero = int(input('Novo número: '))
                 if i['numero']:
                     i['numero'].append(novo_numero)
+                    
+def mostrarAgenda(agenda):
+    if agenda == []:
+        print('Não há conteúdo.\n')
+    else:
+        print('SEUS CONTATOS:')
+        for i in agenda:
+            for k, v in i.items():
+                print(f'{k.title()} - {v}')
+        print('\n')
 
 
 agenda = []
 
 while True:
     mostrarMenu()
-    if escolha_menu == 1:
+    if escolha_menu == 0:
+        mostrarAgenda(agenda)
+        time.sleep(2)
+
+    elif escolha_menu == 1:
         pegarInformacoes()
         incluirNovoTel(dados, numeros, quantidade_telefone, agenda)
+
     elif escolha_menu == 2:
         incluirTel(dados, numeros)
-    elif escolha_menu == 5:
-        print(agenda)
-        break
